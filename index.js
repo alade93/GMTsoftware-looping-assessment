7273b9f649ace52801e6a30738c9cc226f81f892
alade93 <99254792+alade93@users.noreply.github.com>
2022-04-12 01:28:03 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,148 @@
+// const id = Math.random().toString(36).substring(7);
+// console.log(id); //random id
+
+// const countries = [
+//   "ALBANIA",
+//   "BOLIVIA",
+//   "CANADA",
+//   "DENMARK",
+//   "ETHIOPIA",
+//   "FINLAND",
+//   "GERMANY",
+//   "HUNGARY",
+//   "IRELAND",
+//   "JAPAN",
+//   "KENYA",
+// ];
+// const NewArr = [];
+// for (const country of countries) {
+//   NewArr.push(country.length);
+// }
+// console.log(NewArr);
+
+// const newarr = [];
+// for (const country of countries) {
+//   newarr.push(country, country.substring(0, 3), country.length);
+// }
+// console.log(newarr);
+
+// for (i = 0; i < country.length; i++) {
+//   if (country == "land") {
+//     console.log(country[i]);
+//   }
+// }
+// for (i = 0; i < country; i++) {
+//   if (country == "ia") {
+//     console.log(country[i]);
+//   }
+// }
+// for (i = 0; i < country.length; i++) {
+//   if (country > 7) {
+//     console.log(country[i]);
+//   }
+// }
+// for (i = 0; i < country.length; i++) {
+//   if (country == 5) {
+//     console.log(country[i]);
+//   }
+// }
+
+// const webTechs = [
+//   "HTML",
+//   "CSS",
+//   "JavaScript",
+//   "React",
+//   "Redux",
+//   "Node",
+//   "MongoDB",
+// ];
+
+// const mernStack = ["MongoDB", "Express", "React", "Node"];
+
+// for (i = 0; i < webTechs.length; i++) {
+//   console.log(webTechs[i].length);
+// }
+
+// const newarr = [];
+// for (const tech of webTechs) {
+//   newarr.push(tech, tech.length);
+// }
+// console.log(newarr);
+
+// for (const tech of webTechs) {
+//   console.log(tech);
+// }
+
+// const fruits = ["banana", "orange", "mango", "lemon"];
+// for (let i = fruits.length; i > fruits.length; i--) {
+//   console.log(fruits[i]);
+// }
+
+// const fullStack = [
+//   ["HTML", "CSS", "JS", "React"],
+//   ["Node", "Express", "MongoDB"],
+// ];
+// console.log(fullStack.toString(fullStack[0], fullStack[1]));
+
+// const countries = [
+//   "ALBANIA",
+//   "BOLIVIA",
+//   "CANADA",
+//   "DENMARK",
+//   "ETHIOPIA",
+//   "FINLAND",
+//   "GERMANY",
+//   "HUNGARY",
+//   "IRELAND",
+//   "JAPAN",
+//   "KENYA",
+// ];
+
+// for (i = 0; i < countries.length; i++) {
+//   if (countries == "land") {
+//     console.log(countries[i]);
+//   }
+// }
+
+// for (i = 0; i < countries.length; i++) {
+//   if (countries.length >= i) {
+//     console.log(countries[i]);
+//   }
+// }
+
+// for (i = 0; i < countries.length; i++) {
+//   if ((countries.length = 4)) {
+//     console.log(countries[i]);
+//   }
+// }
+
+// for (i = 0; i < countries.length; i++) {
+//   if (countries.length <= 2) {
+//     console.log(countries[i]);
+//   }
+// }
+// const sortedCountries = [];
+// for (const country of countries) {
+//   sortedCountries.sort().push(country);
+// }
+// console.log(sortedCountries);
+
+// countries.reverse();
+// console.log(countries);
+
+// const webTechs = [
+//   "HTML",
+//   "CSS",
+//   "JavaScript",
+//   "React",
+//   "Redux",
+//   "Node",
+//   "MongoDB",
+// ];
+
+// webTechs.sort();
+// console.log(webTechs);
+
+// const mernStack = ["MongoDB", "Express", "React", "Node"];
+// mernStack.sort();
+// console.log(mernStack);
